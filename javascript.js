8fd5600879f83aa176b802685017de3cc5ba5465
lukaseispert <127983335+lukaseispert@users.noreply.github.com>
2023-09-27 09:55:48 +0200

    Add User Interface to it

--- javascript.js
+++ javascript.js
@@ -12,20 +12,20 @@ function getComputerChoice () {
 
 //Funktion für die Wahl des Spielers, wenn Input nicht korrekt wird erneut gefragt
 
- function getPlayerChoice (){
-    let validatedInput = false;
-    while(validatedInput == false) {
-        const choice = prompt("Choose Rock, Paper or Scissors!", "");
-        if(choice == null) {
-            continue;
-        }
-        const choiceInLower = choice.toLowerCase();
-        if(options.includes(choiceInLower)) {
-            validatedInput = true;
-            return choiceInLower;
-        }
-    }
-}
+// function getPlayerChoice (){
+//    let validatedInput = false;
+//   while(validatedInput == false) {
+//        const choice = prompt("Choose Rock, Paper or Scissors!", "");
+//       if(choice == null) {
+ //           continue;
+ //       }
+ //       const choiceInLower = choice.toLowerCase();
+  //      if(options.includes(choiceInLower)) {
+  //          validatedInput = true;
+   //         return choiceInLower;
+   //     }
+   // }
+//}
 
     
     
@@ -56,21 +56,21 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-function game () {
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    console.log("Welcome!")
-    for (let i = 0; i < 5; i++){
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        if(checkWinner (playerSelection, computerSelection) == "Player") {
-            scorePlayer++;
-        } else if (checkWinner(playerSelection, computerSelection) == "Computer") {
-            scoreComputer++;
-        }
+//function game () {
+   // let scorePlayer = 0;
+    //let scoreComputer = 0;
+    //console.log("Welcome!")
+    //for (let i = 0; i < 5; i++){
+    //    const playerSelection = getPlayerChoice();
+    //   const computerSelection = getComputerChoice();
+    //   console.log(playRound(playerSelection, computerSelection));
+    //   if(checkWinner (playerSelection, computerSelection) == "Player") {
+     //       scorePlayer++;
+      //  } else if (checkWinner(playerSelection, computerSelection) == "Computer") {
+     //       scoreComputer++;
+     //   }
 
-    }
+   // }
    
     if (scorePlayer > scoreComputer) {
         console.log ("Congratulations, You Won!");
@@ -80,6 +80,6 @@ function game () {
         console.log("It's a tie, no one is the Winner!");
     }
     
-}
+//}
 
 game()
